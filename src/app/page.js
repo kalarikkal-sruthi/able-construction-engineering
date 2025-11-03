ff9bbe48b14ce7ef853ddd8ddcf79bda6e455adf
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-11-03 16:51:39 +0530

    we dliver smar work

--- src/app/page.js
+++ src/app/page.js
@@ -49,9 +49,9 @@ export default function Home() {
       </section>
       <section className="pl-25 pb-30 pr-25 relative">
         <div className="flex gap-6 items-center">
-          <div className="w-2/5 h-[600px] flex flex-col justify-center " >
+          <div className="w-2/5 h-[600px] flex flex-col justify-center ">
             <h2 className="text-5xl font-bold text-gray-800 mb-6">
-              About 
+              About
               <br></br> <span className="text-[#bf7b7b]">Our Company</span>
             </h2>
             <p className="text-gray-600">
@@ -60,48 +60,104 @@ export default function Home() {
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
-          <p>
- It has
-              survived not only five centuries, but also the leap into
+            <p>
+              It has survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. It was
               popularised in the 1960s with the release of Letraset sheets
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
-          </p>
-          <p>
-            It has
-              survived not only five centuries, but also the leap into
+            </p>
+            <p>
+              It has survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. It was
               popularised in the 1960s with the release of Letraset sheets
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
-          </p>
-            
+            </p>
           </div>
           <div className="w-3/5 relative h-160">
             <Image
               src="/about.jpg"
               alt="About Background"
               fill
-              className="object-contain rounded-2xl"
+              className="object-contain rounded-8xl"
             />
           </div>
         </div>
         <div>
           <Image
-              src="/homelogo.png"
-              alt="About Background"
-              width={500}
-              height={450}
-              
-              className="object-contain absolute start-0 rounded-2xl  -ml-8 "
-            />
+            src="/homelogo.png"
+            alt="About Background"
+            width={500}
+            height={450}
+            className="object-contain absolute start-0 rounded-2xl  -ml-8 "
+          />
         </div>
       </section>
-      <section className="flex flex-col items-start justify-start  text-gray-800  pl-25 pb-20 pr-25">
-
+      <section className="flex flex-col items-center justify-center bg-[#b2b2b1]  text-gray-800  pt-25 pl-25 pb-20 pr-25">
+        <h2 className="text-5xl font-bold text-white mb-6 text-center -mr-12">
+          We deliver
+          <br></br>{" "}
+          <span className="text-[#251f77]">smart building projects</span>
+        </h2>
+      </section>
+      <section className="grid grid-cols-3 gap-20 pl-25 pb-20 pr-25 relative bg-[#b2b2b1] ">
+        <div className="bg-white rounded-xl text-center">
+          {" "}
+          <Image
+            src="/1.jpg"
+            alt="About Background"
+            width={520}
+            height={450}
+            className="object-contain "
+          />
+          <div className="mt-10 mb-10">
+            <h1 className="mb-4 text-black text-4xl ">Riverwalk towers</h1>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+        </div>
+        <div className="bg-white rounded-xl text-center">
+          {" "}
+          <Image
+            src="/2.jpg"
+            alt="About Background"
+            width={520}
+            height={450}
+            className="object-contain  "
+          />
+            <div className="mt-10 mb-10">
+            <h1 className="mb-4 text-black text-4xl ">Riverwalk towers</h1>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+        </div>
+        <div className="bg-white  rounded-xl text-center">
+          {" "}
+          <Image
+            src="/3.jpg"
+            alt="About Background"
+            width={520}
+            height={450}
+            className=" object-contain "
+          />
+            <div className="mt-10 mb-10">
+            <h1 className="mb-4 text-black text-4xl ">Riverwalk towers</h1>
+            <p>
+              {" "}
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+        </div>
       </section>
     </main>
   );
