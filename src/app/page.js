db46759e1aee796dcd32540fb58780c4ad699940
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-11-03 15:14:32 +0530

    about section cleared

--- src/app/page.js
+++ src/app/page.js
@@ -14,17 +14,17 @@ export default function Home() {
           />
         </div>
         <div className="relative z-10 flex flex-col items-start justify-end h-full text-white text-center ml-16 pb-16">
-          <h1 className="text-[5rem] mb-0 mt-0 font-extrabold text-[#251f77] leading-tight ">
+          <h1 className="text-[4rem] mb-0 mt-0 font-extrabold text-[#251f77] leading-tight ">
             P.Y ABLE CONSTRUCTION
           </h1>
-          <h1 className="text-[5rem] mb-0 mt-0 font-extrabold text-[#251f77] leading-tight">
+          <h1 className="text-[4rem] mb-0 mt-0 font-extrabold text-[#251f77] leading-tight">
             & ENGINEERING CO. LTD
           </h1>
-          <p className="text-[3rem] font-extrabold  text-[#fff] mb-0 mt-0">
+          <p className="text-[2rem] font-extrabold  text-[#fff] mb-0 mt-0">
             Private Limited By Shares
           </p>
           <div className="text-center ">
-            <h3 className="text-6xl font-extrabold text-[#251f77] bg-[#ffffff85] rounded-full  flex items-center px-5 py-3 ">
+            <h3 className="text-5xl font-extrabold text-[#251f77] bg-[#ffffff85] rounded-full  flex items-center px-5 py-3 ">
               35+
               <span className="text-xl text-[#d1413f]">
                 Years of Experience
@@ -37,8 +37,8 @@ export default function Home() {
       </section>
       <section className="flex flex-col items-center justify-center  text-gray-800 pt-25 pl-25 pb-15 pr-25">
         <div className="  flex-none ">
-          <h2 className="font-bold text-5xl text/8 text-center">
-            We deliver expert building work
+          <h2 className="font-bold text-6xl text/8 text-center">
+            We deliver expert building work<br></br>
             <span className="text-[#bf7b7b]">
               solutions and trusted support for
             </span>
@@ -52,20 +52,32 @@ export default function Home() {
           <div className="w-2/5 h-[600px] flex flex-col justify-center " >
             <h2 className="text-5xl font-bold text-gray-800 mb-6">
               About 
-              <br></br> Our Company
+              <br></br> <span className="text-[#bf7b7b]">Our Company</span>
             </h2>
             <p className="text-gray-600">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
+              type and scrambled it to make a type specimen book.
+            </p>
+          <p>
+ It has
               survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. It was
               popularised in the 1960s with the release of Letraset sheets
               containing Lorem Ipsum passages, and more recently with desktop
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
-            </p>
+          </p>
+          <p>
+            It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+          </p>
             
           </div>
           <div className="w-3/5 relative h-160">
@@ -81,10 +93,10 @@ export default function Home() {
           <Image
               src="/homelogo.png"
               alt="About Background"
-              width={400}
-              height={400}
+              width={500}
+              height={450}
               
-              className="object-contain absolute start-0 bottom-0 rounded-2xl bottom-0 -ml-8 "
+              className="object-contain absolute start-0 rounded-2xl  -ml-8 "
             />
         </div>
       </section>
