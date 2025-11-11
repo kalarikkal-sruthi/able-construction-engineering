95d18605e7df14dc666bdcbd5edef52f4e04ed1c
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-11-11 10:23:46 +0530

    video 3 added

--- src/componets/ui/BannerSlider.jsx
+++ src/componets/ui/BannerSlider.jsx
@@ -58,7 +58,7 @@ function BannerSlider() {
                 >
                   <div className="mb-3 sm:mb-4 lg:mb-6">
                     <span className="text-3xl sm:text-3xl lg:text-4xl xl:text-5xl font-light font-sans tracking-wide drop-shadow-lg leading-tight">
-                      We have a strong geographic footprint with major projects in India, Uganda, Qatar, the UAE, and Oman.
+                     Construction sector across India, Uganda, Qatar, UAE, Oman
                     </span>
                   </div>
                 </motion.h1>
@@ -121,7 +121,7 @@ function BannerSlider() {
                 >
                   <div className="mb-3 sm:mb-4 lg:mb-6">
                     <span className="text-3xl sm:text-3xl lg:text-4xl xl:text-5xl font-light font-sans tracking-wide drop-shadow-lg leading-tight">
-                      Qatar Saudi Materials Trading
+                      Trading between Qatar and Saudi Arabia
                     </span>
                   </div>
                 </motion.h1>
@@ -149,6 +149,134 @@ function BannerSlider() {
               </div>
             </div>
           </SwiperSlide>
+                <SwiperSlide>
+            <div className="relative h-screen w-full flex items-center justify-center">
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="absolute top-0 left-0 w-full h-full object-cover z-10"
+              >
+                <source src="/hero2.mp4" type="video/mp4" />
+                <source src="/hero2.mp4" type="video/webm" />
+                <Image
+                  src="/main-banner-video-image.png"
+                  alt="Hero Background"
+                  fill
+                  className="object-cover"
+                />
+              </video>
+
+              <div className="absolute top-0 left-0 h-full bg-black z-20"></div>
+
+              {/* Centered Text Content */}
+              <div className="relative z-30 text-center text-white px-4 sm:px-6 lg:px-8 xl:px-12 max-w-4xl mx-auto">
+                <motion.h1
+                  initial={{ opacity: 0, y: -20 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.8,
+                    ease: "easeOut",
+                  }}
+                  viewport={{ once: true, amount: 0.3 }}
+                  className="w-full"
+                >
+                  <div className="mb-3 sm:mb-4 lg:mb-6">
+                    <span className="text-3xl sm:text-3xl lg:text-4xl xl:text-5xl font-light font-sans tracking-wide drop-shadow-lg leading-tight">
+                      Agricultural practices in India and Uganda
+                    </span>
+                  </div>
+                </motion.h1>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: 0.4,
+                    ease: "easeOut",
+                  }}
+                  viewport={{ once: true, amount: 0.3 }}
+                  className="flex flex-col sm:flex-row gap-3 sm:gap-4 lg:gap-6 justify-center items-center"
+                >
+                  <button className="bg-transparent border border-white text-white px-2 xl:px-2 py-2 rounded-full transition-all duration-300 text-sm xl:text-base relative overflow-hidden group hover:-translate-y-0.5 flex items-center justify-center min-w-[135px] xl:min-w-[135px] hover:bg-white/20">
+                    <span className="transition-transform duration-300 group-hover:-translate-x-2">
+                      Get In Touch
+                    </span>
+                    <span className="absolute right-4 top-1.5 opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 font-bold">
+                      →
+                    </span>
+                  </button>
+                </motion.div>
+              </div>
+            </div>
+          </SwiperSlide>
+           <SwiperSlide>
+            <div className="relative h-screen w-full flex items-center justify-center">
+              <video
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="absolute top-0 left-0 w-full h-full object-cover z-10"
+              >
+                <source src="/hero2.mp4" type="video/mp4" />
+                <source src="/hero2.mp4" type="video/webm" />
+                <Image
+                  src="/main-banner-video-image.png"
+                  alt="Hero Background"
+                  fill
+                  className="object-cover"
+                />
+              </video>
+
+              <div className="absolute top-0 left-0 h-full bg-black z-20"></div>
+
+              {/* Centered Text Content */}
+              <div className="relative z-30 text-center text-white px-4 sm:px-6 lg:px-8 xl:px-12 max-w-4xl mx-auto">
+                <motion.h1
+                  initial={{ opacity: 0, y: -20 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.8,
+                    ease: "easeOut",
+                  }}
+                  viewport={{ once: true, amount: 0.3 }}
+                  className="w-full"
+                >
+                  <div className="mb-3 sm:mb-4 lg:mb-6">
+                    <span className="text-3xl sm:text-3xl lg:text-4xl xl:text-5xl font-light font-sans tracking-wide drop-shadow-lg leading-tight">
+                      Where Every Journey Becomes Great Story
+                    </span>
+                  </div>
+                </motion.h1>
+
+                <motion.div
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{
+                    duration: 0.8,
+                    delay: 0.4,
+                    ease: "easeOut",
+                  }}
+                  viewport={{ once: true, amount: 0.3 }}
+                  className="flex flex-col sm:flex-row gap-3 sm:gap-4 lg:gap-6 justify-center items-center"
+                >
+                  <button className="bg-transparent border border-white text-white px-2 xl:px-2 py-2 rounded-full transition-all duration-300 text-sm xl:text-base relative overflow-hidden group hover:-translate-y-0.5 flex items-center justify-center min-w-[135px] xl:min-w-[135px] hover:bg-white/20">
+                    <span className="transition-transform duration-300 group-hover:-translate-x-2">
+                      Get In Touch
+                    </span>
+                    <span className="absolute right-4 top-1.5 opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 font-bold">
+                      →
+                    </span>
+                  </button>
+                </motion.div>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          
         </Swiper>
       </section>
     </diiv>
