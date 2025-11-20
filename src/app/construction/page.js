59ae95dcb486240b5a1a2b55ca0405426dd6c866
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-11-20 10:59:41 +0530

    page cleared

--- src/app/construction/page.js
+++ src/app/construction/page.js
@@ -5,12 +5,14 @@ import { motion } from "framer-motion";
 import KeyServices from "@/componets/ui/KeyServices";
 import ConstructionBanner from "@/componets/ui/ConstructionBanner";
 import OurglobalFootprint from "@/componets/ui/OurglobalFootprint";
+import RegisterInterestForm from "@/componets/ui/RegisterInterestForm";
 export default function page() {
   return (
     <main>
       <ConstructionBanner />
       <KeyServices />
       <OurglobalFootprint />
+      <RegisterInterestForm />
     </main>
   );
 }
--- src/app/trading/page.js
+++ src/app/trading/page.js
@@ -1,5 +1,6 @@
 import OurCoreTradingDivisions from '@/componets/ui/OurCoreTradingDivisions'
 import OurStrategicPresence from '@/componets/ui/OurStrategicPresence'
+import RegisterInterestForm from '@/componets/ui/RegisterInterestForm'
 import TradingBanner from '@/componets/ui/TradingBanner'
 import React from 'react'
 
@@ -9,6 +10,7 @@ function page() {
 <TradingBanner />
 <OurCoreTradingDivisions />
 <OurStrategicPresence />
+<RegisterInterestForm />
 
     </div>
   )
