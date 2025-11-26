00e0976f9db6745b443cb2f1735ddbee9057210d
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-11-26 12:55:36 +0530

    responsive cleared

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,9 +9,9 @@ function page() {
     <div>
       <div className="h-[70vh]  min-h-[500px] lg:min-h-[600px] bg-black flex items-center justify-center px-6 lg:px-24 ">
         <div className="grid grid-cols-5 lg:grid-cols-5  items-center justify-center  ">
-          <div className="col-span-5 lg:col-span-2 bg-gray-900 my-6 p-6 my-6 lg:p-6 text-white  w-full h-full flex items-center ">
+          <div className="col-span-5 lg:col-span-2 bg-gray-900 my-6 p-3 lg:p-6 text-white  w-full h-full flex items-center ">
             <div className="w-full">
-              <div className="text-start mb-4 md:mb-8">
+              <div className="text-start mb-0 md:mb-8">
                 <motion.h1
                   initial={{ opacity: 0, y: -20 }}
                   whileInView={{ opacity: 1, y: 0 }}
