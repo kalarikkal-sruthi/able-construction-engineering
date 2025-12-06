c98676bf8e24f70d4c922381f07e13062e78ad62
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-12-06 12:42:31 +0530

    yaml updated

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,17 @@ const nextConfig = {
   images: {
     domains: [],  // Add your image domains here if needed
   },
+   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  
+  // Disable ESLint during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Enable SWC minification
+  swcMinify: true,
   // Add any other config options here
 };
 
