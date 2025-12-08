72b1245303b4056ae56e85c2434eef2150fb372b
kalarikkal-sruthi <kalarikkalsruthi79@gmail.com>
2025-12-08 13:58:29 +0530

    some others are added for optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  output: 'standalone',  // Remove this if not using Digital Ocean App Platform
-  images: {
-    domains: [],  // Add your image domains here if needed
+  
+  reactCompiler: true ,
+
+
+  swcMinify: true, 
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
   },
-  // Add any other config options here
+ 
+  experimental: {
+    // esmExternals: false,
+    
+    // turbo: true,
+  },
 };
 
 
